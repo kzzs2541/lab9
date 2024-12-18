fbac73588894d498e0f413eb21388ccca8f5716c
kzzs <01197783@pw.edu.pl>
2024-12-18 19:41:56 +0100

    napisanie funkcji podstawienia wstecznego

--- src/backsubst.c
+++ src/backsubst.c
@@ -4,19 +4,30 @@
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
-int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+int backsubst(Matrix *x, Matrix *mat, Matrix *b) {
+    // sprawdzenie zgodnosci wymiarow macierzy
+    if (mat->r != mat->c || b->r != mat->r || b->c != 1 || x->r != mat->r || x->c != 1) {
+        return 2; // blad
+    }
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
+    int n = mat->r; // Rozmiar macierzy A
 
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
+    // iteracja wsteczna
+    for (int i = n - 1; i >= 0; i--) {
+        // sprawdzenie elementu zerowego
+        if (mat->data[i][i] == 0) {
+            return 1; // blad dzielenia przez 0
+        }
 
-				return 0;
+        double sum = 0.0;
+        // Liczymy sumę dla znanych już wartości niewiadomych
+        for (int j = i + 1; j < n; j++) {
+            sum += mat->data[i][j] * x->data[j][0];
+        }
+
+        // Obliczamy wartość x[i]
+        x->data[i][0] = (b->data[i][0] - sum) / mat->data[i][i];
+    }
+
+    return 0; // udane wyliczenie
 }
-
-
