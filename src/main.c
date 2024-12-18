0f3cd3a5941031abf9d976148e3dba0d5b6be012
kzzs <01197783@pw.edu.pl>
2024-12-18 19:41:12 +0100

    obsluga backsubst i obsluga bledow

--- src/main.c
+++ src/main.c
@@ -4,27 +4,43 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
-int main(int argc, char ** argv) {
+int main(int argc, char **argv)
+{
 	int res;
-	Matrix * A = readFromFile(argv[1]);
-	Matrix * b = readFromFile(argv[2]);
-	Matrix * x;
+	Matrix *A = readFromFile(argv[1]);
+	Matrix *b = readFromFile(argv[2]);
+	Matrix *x;
 
-	if (A == NULL) return -1;
-	if (b == NULL) return -2;
+	if (A == NULL)
+		return -1;
+	if (b == NULL)
+		return -2;
+	printf("Macierz A:\n");
 	printToScreen(A);
+	printf("Macierz b:\n");
 	printToScreen(b);
 
-	res = eliminate(A,b);
-	x = createMatrix(b->r, 1);
-	if (x != NULL) {
-		res = backsubst(x,A,b);
+	res = eliminate(A, b);
 
+	if (res == 1)
+	{
+		fprintf(stderr, "Nie udalo sie zredukowac macierzy - macierz osobliwa\n");
+	}
+
+	x = createMatrix(b->r, 1);
+	if (x != NULL)
+	{
+		res = backsubst(x, A, b);
+	}
+	if (res == 0)
+	{
+		printf("Macierz wynikowa x:\n");
 		printToScreen(x);
-	  freeMatrix(x);
-	} else {
-					fprintf(stderr,"Błąd! Nie mogłem utworzyć wektora wynikowego x.\n");
+		freeMatrix(x);
+	}
+	else
+	{
+		fprintf(stderr, "Błąd! Nie mogłem utworzyć wektora wynikowego x.\n");
 	}
 
 	freeMatrix(A);
